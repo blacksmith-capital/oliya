b0a9e0c4c05a2658f472c2e3d63ccab3e16396f7
Yuri Kovalov <yuri@yurikoval.com>
2020-05-17 12:48:14 +0800

    Remove unused comments

--- mix.exs
+++ mix.exs
@@ -15,9 +15,6 @@ defmodule Oliya.MixProject do
     ]
   end
 
-  # Configuration for the OTP application.
-  #
-  # Type `mix help compile.app` for more information.
   def application do
     [
       mod: {Oliya.Application, []},
@@ -25,13 +22,9 @@ defmodule Oliya.MixProject do
     ]
   end
 
-  # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
   defp elixirc_paths(_), do: ["lib"]
 
-  # Specifies your project dependencies.
-  #
-  # Type `mix help deps` for examples and options.
   defp deps do
     [
       {:ex_tectonicdb, "~> 0.1.0"},
