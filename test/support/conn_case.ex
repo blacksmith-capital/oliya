07c1dd68cd55db60e7ceae4714963c0f3a4671a5
Yuri Kovalov <yuri@yurikoval.com>
2023-07-14 09:48:37 +0800

    Fix phoenix deprecation

--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -18,7 +18,8 @@ defmodule OliyaWeb.ConnCase do
   using do
     quote do
       # Import conveniences for testing with connections
-      use Phoenix.ConnTest
+      import Plug.Conn
+      import Phoenix.ConnTest
       alias OliyaWeb.Router.Helpers, as: Routes
 
       # The default endpoint for testing
