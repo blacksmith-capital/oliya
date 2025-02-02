f19c6ed8ac6780b34970bdf67d00eb1d15100683
Yuri Kovalov <yuri@yurikoval.com>
2025-02-02 09:37:42 +0800

    Update Gettext interface

--- lib/oliya_web/gettext.ex
+++ lib/oliya_web/gettext.ex
@@ -20,5 +20,5 @@ defmodule OliyaWeb.Gettext do
 
   See the [Gettext Docs](https://hexdocs.pm/gettext) for detailed usage.
   """
-  use Gettext, otp_app: :oliya
+  use Gettext.Backend, otp_app: :oliya
 end
